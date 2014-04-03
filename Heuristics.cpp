d2ae12a647c6a36556fecf5336bfebe71d8b76c1
chrisstopher <chrisstophervan@gmail.com>
2014-04-03 15:10:08 -0700

    Created heuristic strategies and factory

--- Heuristics.cpp
+++ Heuristics.cpp
@@ -1,25 +1,69 @@
 #include "Heuristics.h"
 
 /**
- * @brief getMovementCost x and y cannot be 0 at the same time
+ * @brief Heuristic::getMovementCost x and y cannot be 0 at the same time
  * @param x can only be -1, 0, 1
  * @param y can only be -1, 0, 1
  * @return if its a diagnol node returns the diagnol cost else it is a horizontal or vertical node so return that cost
  */
-int getMovementCost(int x, int y) {
+int Heuristic::getMovementCost(int x, int y) {
     if (x == 0 || y == 0) {
         return HORIZONTAL_OR_VERTICAL;
     }
     return DIAGNOL;
 }
+
 /**
- * @brief getManhattanDistance
+ * @brief ManhattanHeuristic::getDistance
  * @param startPosition - current node position in map
  * @param endPosition - desired node position in map
- * @return manhattan distance between 2 points times the HORIZONTAL_OR_VERTICAL movement
+ * @return HORIZONTAL_OR_VERTICAL movement times the absolute between the x's and y's added together
  */
-int getManhattanDistance(const Vec2i& startPosition, const Vec2i& endPosition) {
+int ManhattanHeuristic::getDistance(const Vec2i& startPosition, const Vec2i& endPosition) {
     return HORIZONTAL_OR_VERTICAL
            * (std::abs(startPosition.getX() - endPosition.getX())
            + std::abs(startPosition.getY() - endPosition.getY()));
+}
+
+/**
+ * @brief ChebyshevHeuristic::getDistance
+ * @param startPosition - current node position in map
+ * @param endPosition - desired node position in map
+ * @return HORIZONTAL_OR_VERTICAL movement times the max absolute between the x's and y's
+ */
+int ChebyshevHeuristic::getDistance(const Vec2i& startPosition, const Vec2i& endPosition) {
+    return HORIZONTAL_OR_VERTICAL
+           * std::max(std::abs(startPosition.getX() - endPosition.getX()),
+                      std::abs(startPosition.getY() - endPosition.getY()));
+}
+
+/**
+ * @brief EuclideanHeuristic::getDistance
+ * @param startPosition - current node position in map
+ * @param endPosition - desired node position in map
+ * @return HORIZONTAL_OR_VERTICAL movement times the magnitude
+ */
+int EuclideanHeuristic::getDistance(const Vec2i& startPosition, const Vec2i& endPosition) {
+    int deltaX = std::abs(startPosition.getX() - endPosition.getX());
+    int deltaY = std::abs(startPosition.getY() - endPosition.getY());
+    return HORIZONTAL_OR_VERTICAL * std::sqrt(deltaX * deltaX + deltaY * deltaY);
+}
+
+/**
+ * @brief HeuristicsFactory::getHeuristic
+ * if you specify a type outside the bounds it returns manhattan by default
+ * @param type
+ * @return unique pointer with the heuristic depending on the type
+ */
+std::unique_ptr<Heuristic> HeuristicsFactory::getHeuristic(HEURISTIC_TYPE type) {
+    switch (type) {
+        case MANHATTAN:
+            return std::unique_ptr<Heuristic>(new ManhattanHeuristic);
+        case CHEBYSHEV:
+            return std::unique_ptr<Heuristic>(new ChebyshevHeuristic);
+        case EUCLIDEAN:
+            return std::unique_ptr<Heuristic>(new EuclideanHeuristic);
+        default:
+            return std::unique_ptr<Heuristic>(new ManhattanHeuristic);
+    }
 }
--- Heuristics.h
+++ Heuristics.h
@@ -2,9 +2,43 @@
 #define HEURISTICS_H
 
 #include "Vec2i.h"
+#include <memory>
+#include <cmath>
 
-static const int DIAGNOL = 14, HORIZONTAL_OR_VERTICAL = 10;
-int getMovementCost(int x, int y);
-int getManhattanDistance(const Vec2i& startPosition, const Vec2i& endPosition);
+class Heuristic {
+    public:
+        int getMovementCost(int x, int y);
+        virtual int getDistance(const Vec2i& startPosition, const Vec2i& endPosition) = 0;
+    protected:
+        enum MOVEMENT_SCORE {
+            HORIZONTAL_OR_VERTICAL = 10,
+            DIAGNOL = 14
+        };
+};
+
+class ManhattanHeuristic : public Heuristic {
+    public:
+        int getDistance(const Vec2i& startPosition, const Vec2i& endPosition);
+};
+
+class ChebyshevHeuristic : public Heuristic {
+    public:
+        int getDistance(const Vec2i& startPosition, const Vec2i& endPosition);
+};
+
+class EuclideanHeuristic : public Heuristic {
+    public:
+        int getDistance(const Vec2i& startPosition, const Vec2i& endPosition);
+};
+
+class HeuristicsFactory {
+    public:
+        enum HEURISTIC_TYPE {
+            MANHATTAN = 0,
+            CHEBYSHEV,
+            EUCLIDEAN
+        };
+        std::unique_ptr<Heuristic> getHeuristic(HEURISTIC_TYPE type);
+};
 
 #endif // HEURISTICS_H
